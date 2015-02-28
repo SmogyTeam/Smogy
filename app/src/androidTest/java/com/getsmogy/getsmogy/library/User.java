c740bcec3096fc0779fc132f96c3641a4a3b160e
glaca98xd <sawickas2@o2.pl>
2015-02-28 21:03:41 +0100

    First Version of Smogy.
    Thats all for today :)

---
+++ app/src/androidTest/java/com/getsmogy/getsmogy/library/User.java
@@ -0,0 +1,14 @@
+package com.getsmogy.getsmogy.library;
+
+/**
+ * Created by cehaudeigrek on 2015-02-28.
+ */
+public class User {
+
+    private static String lURL = "http://getsmogy.diveam.net/smogy_login_api/";
+
+    public void loginUser() {
+
+    }
+
+}
